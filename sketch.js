536e88a0251d26e78937113e3ed12dcfa1ccbbdb
raghunandan15102009 <73269469+raghunandan15102009@users.noreply.github.com>
2020-12-08 11:07:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,49 @@
+var dog, totalfood, database, food=20;
+
+function preload() {
+  dogImage = loadImage("images/dogImg.png");
+  dogImage1 = loadImage("images/dogImg1.png")
+}
+
+function setup() {
+  createCanvas(500, 500);
+  database = firebase.database();
+  totalfood = database.ref('food');
+  totalfood.on("value", readStock);
+  dog = createSprite(250, 250, 50, 50);
+  dog.addImage(dogImage);
+  dog.scale = 0.3;
+}
+
+
+function draw() {
+  background(46, 139, 87);
+  drawSprites();
+  textSize(20);
+  fill("white");
+  text("Press up arrow to feed the dog",50,450);
+  text("food:"+food,100,100);
+  if (keyDown(UP_ARROW)) {
+    writeStock(food);
+    dog.addImage(dogImage1);
+  }
+
+}
+
+function readStock(data) {
+  food = data.val();
+}
+
+function writeStock(food) {
+  if (food <= 0) {
+    food = 0;
+  } else {
+    food = food - 1;
+  }
+  database.ref('/').update({
+    food: food
+  })
+}
+
+
+
