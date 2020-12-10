a4f6ad18cae1a280a5b675ac3d07c0e11ae3f411
raghunandan15102009 <73269469+raghunandan15102009@users.noreply.github.com>
2020-12-10 09:01:12 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
 var dog, totalfood, database, food=20;
 
 function preload() {
-  dogImage = loadImage("images/dogImg.png");
-  dogImage1 = loadImage("images/dogImg1.png")
+  dogImage = loadImage("dogImg.png");
+  dogImage1 = loadImage("dogImg1.png")
 }
 
 function setup() {
